7e485cf1e076f4b789c4a8e65548663b8c5e4727
Giorgi Alvarez <giorgi@mbp.home>
2022-04-02 19:14:43 -0400

    updated comments microservice

--- posts/index.js
+++ posts/index.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const {randomBytes} = require('crypto');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+
 const app = express();
-
-
 app.use(express.json());
+app.use(cors());
 
 const posts = {};
-app.get('/posts', (req,res)=>{
-    res.status(200).json(posts);
-})
 
-app.post('/posts', (req,res)=>{
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
 
-    posts[id] = {
-        id, title
-    }
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    res.status(201).json(posts[id]);
-})
+  posts[id] = {
+    id,
+    title,
+  };
 
+  res.status(201).json(posts[id]);
+});
 
-app.listen(4000, ()=>{
-    console.log('Listening on port 4000')
-})
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
+});
