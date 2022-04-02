a1cd104392355ce1739cc21f48ea9bd33d11567b
Giorgi Alvarez <giorgi@mbp.home>
2022-04-02 18:14:38 -0400

    made posts and comments modules and updated frontend

---
+++ client/src/PostCreate.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
+import classes from "./App.module.scss";
+
+function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // send a post request to our post service
+    await axios.post("http://192.168.1.4:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
+
+  return (
+    <div className={classes.postCreate}>
+      <form onSubmit={handleSubmit}>
+        <div className={classes.formGroup}>
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(() => e.target.value)}
+            className={classes.formControl}
+            placeholder="Post Title"
+          />
+          <button type="submit" className={classes.buttonPrimary}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default PostCreate;
